cbc751ce86103c2d6b16a2dad731dd07cb2a0776
Rahman E <rahman.erturk@gmail.com>
2022-11-12 12:17:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Car from "./components/Car";
+import { useReducer } from "react";
+
+const initState = {
+  isSwitchedOn: false,
+  speed: 0,
+};
+
+const reducer = (state, action) => {
+  swit;
+  return state;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
+
   return (
     <div className="App">
-      <Car />
+      <Car state={state} dispatch={dispatch} />
     </div>
   );
 }
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,5 +1,16 @@
 import React from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+import Button from "react-bootstrap/Button";
 
-export default function Car() {
-  return <div className="car">Make your instrument cluster here</div>;
+export default function Car({ state, dispatch }) {
+  return (
+    <div className="car">
+      {state.isSwitchedOn && <ReactSpeedometer />}
+      <Button onClick={() => dispatch({ type: "switchOn/Off" })}>
+        {state.isSwitchedOn ? "Ausschalten" : "Anschalten"}
+      </Button>
+      <Button>Gas geben</Button>
+      <Button>Bremsen</Button>
+    </div>
+  );
 }
